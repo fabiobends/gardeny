44934e6b0ee989d35e5b6b10d5d70568e70879ec
Fábio Mendes <fabiobends@gmail.com>
2023-03-01 16:55:11 -0300

    feat: add git hooks to the project
    Add rules for pre-commit and pre-push.

---
+++ .githooks/commit_message.dart
@@ -0,0 +1,20 @@
+import 'dart:io';
+
+final regExp = RegExp(
+  r'(fix|feat|chore|refactor|docs|style|test)\:.+',
+);
+
+void main() {
+  final rootDir = Directory.current;
+  final commitFile = File(rootDir.path + "/.git/COMMIT_EDITMSG");
+  final commitMessage = commitFile.readAsStringSync();
+
+  final isValid = regExp.hasMatch(commitMessage);
+  if (!isValid) {
+    print('''👎 Bad commit message! A correct one would be: 
+        docs: correct spelling of CHANGELOG''');
+    exitCode = 1;
+  } else {
+    print('''🚀 Nice commit message!''');
+  }
+}

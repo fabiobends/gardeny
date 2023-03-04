1f2a5826c76a73e09133e4ca54d5a716de5ed2ef
Fábio Mendes <fabiobends@gmail.com>
2023-03-04 19:15:39 -0300

    feat: simplify app setup when using widgetbook

--- lib/core/routes.dart
+++ lib/core/routes.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:gardeny/core/screens/play_ground_screen.dart';
+import 'package:gardeny/core/widget_book/root.dart';
 
 enum Routes {
   playGround,
+  widgetBook,
 }
 
 Map<String, Widget Function(BuildContext)> getRoutes(BuildContext context) {
   return {
     Routes.playGround.name: (context) => const PlayGroundScreen(),
+    Routes.widgetBook.name: (context) => const WidgetBookScreen(),
   };
 }

eb368ffac56e37cb4e6dfed7349ac4795af47e9e
Fábio Mendes <fabiobends@gmail.com>
2023-03-10 17:26:28 -0300

    feat(content_card): wip
    This commits creates the logic of a general card and starts
    the plant content card.

--- lib/core/screens/play_ground_screen.dart
+++ lib/core/screens/play_ground_screen.dart
@@ -39,7 +39,7 @@ class _PlayGroundScreenState extends State<PlayGroundScreen> {
               Button(
                 onPressed: () {},
                 label: Tr.playGround,
-              )
+              ),
             ],
           ),
         ),
